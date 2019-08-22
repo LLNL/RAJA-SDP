23bafa575be4388f7fc7cc28e384bfbfb6097e7f
mdavis36 <davis291@llnl.gov>
2019-08-22 15:07:16 -0700

    Passing memory to lambda

--- src/forall.hpp
+++ src/forall.hpp
@@ -62,10 +62,10 @@ struct gpu {
 //#endif
 
 template <typename LOOP_BODY>
-void forall_kernel_cpu(int begin, int end, LOOP_BODY body)
+void forall_kernel_cpu(int begin, int end, LOOP_BODY body, float * mem)
 {
   for (int i = 0; i < (end - begin); ++i) {
-    body(i);
+    body(i, gridDim.x, blockDim.x, end, mem);
   }
 }
 
@@ -73,7 +73,7 @@ void forall_kernel_cpu(int begin, int end, LOOP_BODY body)
  * \brief Run forall kernel on CPU.
  */
 template <typename LOOP_BODY>
-void forall(sequential, int begin, int end, LOOP_BODY body)
+void forall(sequential, int begin, int end, LOOP_BODY body, float * mem)
 {
 //  chai::ArrayManager* rm = chai::ArrayManager::getInstance();
 
@@ -83,19 +83,19 @@ void forall(sequential, int begin, int end, LOOP_BODY body)
 
 //  rm->setExecutionSpace(chai::CPU);
 
-  forall_kernel_cpu(begin, end, body);
+  forall_kernel_cpu(begin, end, body, mem);
 
 //  rm->setExecutionSpace(chai::NONE);
 }
 
 //#if defined(CHAI_ENABLE_CUDA) || defined(CHAI_ENABLE_HIP)
 template <typename LOOP_BODY>
-__global__ void forall_kernel_gpu(int start, int length, LOOP_BODY body)
+__global__ void forall_kernel_gpu(int start, int length, LOOP_BODY body, float * mem)
 {
   int idx = blockDim.x * blockIdx.x + threadIdx.x;
 
   if (idx < length) {
-    body(idx);
+    body(idx, gridDim.x, blockDim.x, length, mem);
   }
 }
 
@@ -103,7 +103,7 @@ __global__ void forall_kernel_gpu(int start, int length, LOOP_BODY body)
  * \brief Run forall kernel on GPU.
  */
 template <typename LOOP_BODY>
-void forall(camp::devices::Cuda dev, int begin, int end, LOOP_BODY&& body)
+void forall(camp::devices::Cuda dev, int begin, int end, LOOP_BODY&& body, float * mem)
 {
 //  chai::ArrayManager* rm = chai::ArrayManager::getInstance();
 
@@ -113,8 +113,9 @@ void forall(camp::devices::Cuda dev, int begin, int end, LOOP_BODY&& body)
   size_t gridSize = (end - begin + blockSize - 1) / blockSize;
 
 //#if defined(CHAI_ENABLE_CUDA)
-  forall_kernel_gpu<<<gridSize, blockSize, 0, dev.get_stream()>>>(begin, end - begin, body);
-  cudaDeviceSynchronize();
+  forall_kernel_gpu<<<gridSize, blockSize, 0, dev.get_stream()>>>(begin, end - begin, body, mem);
+  //forall_kernel_gpu<<<G_SIZE, B_SIZE, 0, dev.get_stream()>>>(begin, end - begin, body);
+//  cudaDeviceSynchronize();
 //#elif defined(CHAI_ENABLE_HIP)
 //  hipLaunchKernelGGL(forall_kernel_gpu, dim3(gridSize), dim3(blockSize), 0,0,
 //                     begin, end - begin, body);
--- src/main.cu
+++ src/main.cu
@@ -1,40 +1,62 @@
 #include <iostream>
 #include <typeinfo>
+#include <future>
 #include "forall.hpp"
 
 #define HOST_DEVICE __host__ __device__
 
-using namespace std;
+#define B_SIZE 32
+#define G_SIZE ((1 << 20) + B_SIZE - 1) / B_SIZE
+
+template <typename LOOP_BODY>
+__global__ void forall_kernel_gpu2(int start, int length, LOOP_BODY body, float * mem)
+{
+  int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+  if (idx < length) {
+    body(idx, mem);
+  }
+}
 
 int main(int argc, char *argv[])
 {
   int option = 0;
   if (argc > 1) option = atoi(argv[1]);  
 
-  auto lambda = [] HOST_DEVICE (int tid) {
-    int sum = 0;
-    for (int i = 0; i < tid; i++){
-      sum += sqrt(pow(3.14159,i));
+  auto lambda = [] HOST_DEVICE (int tid, int grid, int block, int N, float * x) {
+    for (int i = tid; i < N; i+= block * grid){
+      x[i] = sqrt(pow(3.14159,i));
     }
   };
 
-  
+  camp::devices::Cuda cudaDevice;
+  camp::devices::Cuda cudaDevice2;
+
+  int N = 1 << 20; 
+
+  float * m1 = cudaDevice.allocate<float>(N);
+  float * m2 = cudaDevice2.allocate<float>(N);
+
   switch(option){ 
     case 0:
-      std::cout << "Running Sequentially" << std::endl;
-      sequential s;
-      forall(s, 0, 30000, lambda);
+      //std::cout << "Running Sequentially" << std::endl;
+      //sequential s;
+      //forall(s, 0, 30000, lambda, m1);
       break;
     case 1:
       std::cout << "Running On GPU" << std::endl;
       
-      camp::devices::Cuda cudaDevice;
-      forall(cudaDevice, 0, 30000, lambda);
+      forall(cudaDevice, 0, N, lambda, m1);
 
-      camp::devices::Cuda cudaDevice2;
-      forall(cudaDevice2, 0, 30000, lambda);
+      forall(cudaDevice2, 0, N, lambda, m2);
 
-      forall(cudaDevice, 0, 30000, lambda);
+      forall(cudaDevice, 0, N, lambda, m1);
+      break;
+    case 2:
+      //std::cout << "Raw CUDA Calls" << std::endl;
+      //forall_kernel_gpu2<<<G_SIZE, B_SIZE >>>(0, N, lambda, m1);
+      //forall_kernel_gpu2<<<G_SIZE, B_SIZE, 0, cudaDevice2.get_stream()>>>(0, N, lambda, m1);
+      //forall_kernel_gpu2<<<G_SIZE, B_SIZE, 0, cudaDevice.get_stream()>>>(0, N, lambda, m1);
       break;
   }
 
